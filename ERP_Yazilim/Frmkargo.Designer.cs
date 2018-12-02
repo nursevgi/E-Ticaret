526c0494b7e592ed291b04d366c63c495f0a2f95
nursevgi <sevgi_nur.1785@hotmail.com>
2018-12-02 17:31:05 +0300

    kargo

--- ERP_Yazilim/Frmkargo.Designer.cs
+++ ERP_Yazilim/Frmkargo.Designer.cs
@@ -28,11 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frmkargo));
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btnkaydet = new System.Windows.Forms.Button();
+            this.btnyeni = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtkargoadi = new System.Windows.Forms.TextBox();
+            this.txtkargoid = new System.Windows.Forms.TextBox();
+            this.txtsube = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtadres = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgkargolar = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgkargolar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnsil
+            // 
+            this.btnsil.BackColor = System.Drawing.Color.White;
+            this.btnsil.Image = ((System.Drawing.Image)(resources.GetObject("btnsil.Image")));
+            this.btnsil.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnsil.Location = new System.Drawing.Point(522, 239);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(79, 62);
+            this.btnsil.TabIndex = 5;
+            this.btnsil.Text = "SİL";
+            this.btnsil.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnsil.UseVisualStyleBackColor = false;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // btnkaydet
+            // 
+            this.btnkaydet.BackColor = System.Drawing.Color.White;
+            this.btnkaydet.Image = ((System.Drawing.Image)(resources.GetObject("btnkaydet.Image")));
+            this.btnkaydet.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnkaydet.Location = new System.Drawing.Point(522, 145);
+            this.btnkaydet.Name = "btnkaydet";
+            this.btnkaydet.Size = new System.Drawing.Size(79, 62);
+            this.btnkaydet.TabIndex = 3;
+            this.btnkaydet.Text = "KAYDET";
+            this.btnkaydet.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnkaydet.UseVisualStyleBackColor = false;
+            this.btnkaydet.Click += new System.EventHandler(this.btnkaydet_Click);
+            // 
+            // btnyeni
+            // 
+            this.btnyeni.BackColor = System.Drawing.Color.White;
+            this.btnyeni.Image = ((System.Drawing.Image)(resources.GetObject("btnyeni.Image")));
+            this.btnyeni.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnyeni.Location = new System.Drawing.Point(522, 48);
+            this.btnyeni.Name = "btnyeni";
+            this.btnyeni.Size = new System.Drawing.Size(79, 62);
+            this.btnyeni.TabIndex = 4;
+            this.btnyeni.Text = "YENİ";
+            this.btnyeni.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnyeni.UseVisualStyleBackColor = false;
+            this.btnyeni.Click += new System.EventHandler(this.btnyeni_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(51, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "KARGO ADI:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "ID:";
+            this.label1.Visible = false;
+            // 
+            // txtkargoadi
+            // 
+            this.txtkargoadi.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtkargoadi.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtkargoadi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtkargoadi.Location = new System.Drawing.Point(172, 48);
+            this.txtkargoadi.Name = "txtkargoadi";
+            this.txtkargoadi.Size = new System.Drawing.Size(323, 27);
+            this.txtkargoadi.TabIndex = 0;
+            this.txtkargoadi.TextChanged += new System.EventHandler(this.txtkargoadi_TextChanged);
+            // 
+            // txtkargoid
+            // 
+            this.txtkargoid.Enabled = false;
+            this.txtkargoid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtkargoid.Location = new System.Drawing.Point(48, 12);
+            this.txtkargoid.Name = "txtkargoid";
+            this.txtkargoid.Size = new System.Drawing.Size(34, 27);
+            this.txtkargoid.TabIndex = 20;
+            this.txtkargoid.TabStop = false;
+            this.txtkargoid.Visible = false;
+            // 
+            // txtsube
+            // 
+            this.txtsube.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtsube.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtsube.Location = new System.Drawing.Point(172, 115);
+            this.txtsube.Name = "txtsube";
+            this.txtsube.Size = new System.Drawing.Size(323, 27);
+            this.txtsube.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(65, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "ŞUBE ADI:";
+            // 
+            // txtadres
+            // 
+            this.txtadres.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtadres.Location = new System.Drawing.Point(172, 159);
+            this.txtadres.Multiline = true;
+            this.txtadres.Name = "txtadres";
+            this.txtadres.Size = new System.Drawing.Size(323, 142);
+            this.txtadres.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(86, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "ADRES:";
+            // 
+            // dgkargolar
+            // 
+            this.dgkargolar.AllowUserToAddRows = false;
+            this.dgkargolar.AllowUserToDeleteRows = false;
+            this.dgkargolar.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgkargolar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgkargolar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgkargolar.Location = new System.Drawing.Point(0, 371);
+            this.dgkargolar.Name = "dgkargolar";
+            this.dgkargolar.ReadOnly = true;
+            this.dgkargolar.RowTemplate.Height = 24;
+            this.dgkargolar.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgkargolar.Size = new System.Drawing.Size(627, 386);
+            this.dgkargolar.TabIndex = 6;
+            this.dgkargolar.DoubleClick += new System.EventHandler(this.dgkargolar_DoubleClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(11, 348);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(232, 20);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "KAYITLI KARGO FİRMALARI:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(182, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(300, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Kargo Adına Göre Filtreli Arama Yapılmaktadır!";
+            // 
+            // Frmkargo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Frmkargo";
+            this.ClientSize = new System.Drawing.Size(627, 757);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgkargolar);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.btnkaydet);
+            this.Controls.Add(this.btnyeni);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtadres);
+            this.Controls.Add(this.txtsube);
+            this.Controls.Add(this.txtkargoadi);
+            this.Controls.Add(this.txtkargoid);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(645, 804);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(645, 804);
+            this.Name = "Frmkargo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kargo Firmaları";
+            this.Load += new System.EventHandler(this.Frmkargo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgkargolar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnkaydet;
+        private System.Windows.Forms.Button btnyeni;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtkargoadi;
+        private System.Windows.Forms.TextBox txtkargoid;
+        private System.Windows.Forms.TextBox txtsube;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtadres;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgkargolar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
--- ERP_Yazilim/Frmkargo.cs
+++ ERP_Yazilim/Frmkargo.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace ERP_Yazilim
 {
@@ -16,5 +18,217 @@ namespace ERP_Yazilim
         {
             InitializeComponent();
         }
+
+        SqlCommand command = new SqlCommand();
+        SqlConnection conn = new SqlConnection();
+
+        public void baglan()
+        {
+            conn.Close();
+            conn.ConnectionString = "server=.; Initial Catalog=erp;Integrated Security=SSPI";
+            command.Connection = conn;
+            command.Parameters.Clear();
+            conn.Open();
+        }
+
+        void listele()
+        {
+            baglan();
+            command.CommandText = "SELECT * FROM kargo ORDER BY kargoadi";
+            DataTable tablo = new DataTable();
+            tablo.Load(command.ExecuteReader());
+            dgkargolar.DataSource = tablo;
+            dgkargolar.Columns["kargono"].Visible = false;
+            dgkargolar.Columns["kargoadi"].HeaderText = "KARGO ADI";
+            dgkargolar.Columns["sube"].HeaderText = "ŞUBE";
+            dgkargolar.Columns["adres"].HeaderText = "ADRES";
+            conn.Close();
+        }
+
+        private void Frmkargo_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        void eklekargo()
+        {
+            baglan();
+            command.CommandText = "INSERT INTO kargo (kargoadi,sube,adres) VALUES (@kargoadi,@sube,@adres)";
+            command.Parameters.AddWithValue("@kargoadi", txtkargoadi.Text);
+            command.Parameters.AddWithValue("@sube", txtsube.Text);
+            command.Parameters.AddWithValue("@adres", txtadres.Text);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Kargo Firması Başarıyla Eklendi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            conn.Close();
+
+            baglan();
+            command.CommandText = "SELECT MAX(kargono) FROM kargo";
+            txtkargoid.Text = command.ExecuteScalar().ToString();
+            conn.Close();
+            listele();
+        }
+
+        void guncellekargo()
+        {
+            baglan();
+            command.CommandText = "UPDATE kargo SET kargoadi=@kargoadi,sube=@sube,adres=@adres WHERE kargono=@kargono";
+            command.Parameters.AddWithValue("@kargoadi", txtkargoadi.Text);
+            command.Parameters.AddWithValue("@sube", txtsube.Text);
+            command.Parameters.AddWithValue("@adres", txtadres.Text);
+            command.Parameters.AddWithValue("@kargono", Convert.ToInt32(txtkargoid.Text));
+            command.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Kargo Firması Bilgileri Başarıyla Güncellendi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele();
+        }
+
+        void Kaydetkargo()
+        {
+            baglan();
+            command.CommandText = "SELECT COUNT(*) FROM kargo WHERE kargono=@kargono";
+            command.Parameters.AddWithValue("@kargono", txtkargoid.Text);
+
+            int sayi = Convert.ToInt32(command.ExecuteScalar());
+
+            if (sayi > 0)
+            {
+                DialogResult cevap = MessageBox.Show("Kargo Firmasına Ait Kayıt Güncellenecektir.İşleme Devam Etmek İstiyor Musunuz?", "UYARI", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (cevap == DialogResult.Yes)
+                {
+                    guncellekargo();
+                }
+                else
+                {
+                    MessageBox.Show("Kargo Firması Güncelleme İşlemi İptal Edildi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    listele();
+                }
+            }
+            else
+            {
+                eklekargo();
+            }
+        }
+
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+            if (txtkargoadi.Text != "")
+            {
+                if (FrmAnaktg.SayisalMi(txtkargoadi.Text) == false)
+                {
+                    if (txtsube.Text != "")
+                    {
+                        if (FrmAnaktg.SayisalMi(txtsube.Text) == false)
+                        {
+                            Kaydetkargo();
+                        }
+
+                        else
+                        {
+                            MessageBox.Show("Şube Adı Sayısal Değer Olamaz!", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            txtsube.Focus();
+                            txtsube.SelectAll();
+                        }
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("Şube Adı Boş Geçilemez!", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        txtsube.Focus();
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("Kargo Firması Adı Sayısal Değer Olamaz!", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtkargoadi.Focus();
+                    txtkargoadi.SelectAll();
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Kargo Firması Adı Boş Geçilemez!", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtkargoadi.Focus();
+            }
+        }
+
+        void temizle()
+        {
+            txtkargoadi.Clear();
+            txtsube.Clear();
+            txtadres.Clear();
+            txtkargoid.Clear();
+            txtkargoadi.Focus();
+        }
+
+        private void btnyeni_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+
+        private void txtkargoadi_TextChanged(object sender, EventArgs e)
+        {
+            baglan();
+            command.CommandText = "SELECT * FROM kargo WHERE kargoadi LIKE @kargoadi ORDER BY kargono";
+            command.Parameters.AddWithValue("@kargoadi", "%" + txtkargoadi.Text + "%");
+
+            DataTable tablo = new DataTable();
+            tablo.Load(command.ExecuteReader());
+            dgkargolar.DataSource = tablo;
+            conn.Close();
+        }
+
+        private void dgkargolar_DoubleClick(object sender, EventArgs e)
+        {
+            baglan();
+            command.CommandText = "SELECT * FROM kargo WHERE kargono=@kargono";
+            command.Parameters.AddWithValue("@kargono", dgkargolar.CurrentRow.Cells[0].Value.ToString());
+
+            DataTable tablo = new DataTable();
+            tablo.Load(command.ExecuteReader());
+
+            txtkargoid.Text = tablo.Rows[0]["kargono"].ToString();
+            txtkargoadi.Text = tablo.Rows[0]["kargoadi"].ToString();
+            txtsube.Text = tablo.Rows[0]["sube"].ToString();
+            txtadres.Text = tablo.Rows[0]["adres"].ToString();
+            conn.Close();
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            Frmilce ilce = new Frmilce();
+            baglan();
+            DialogResult cevap = MessageBox.Show("Seçilen Kargo Firması Kaydı Silinecektir.İşleme Devam Etmek İstiyor Musunuz?", "UYARI", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (cevap == DialogResult.Yes)
+            {
+                
+                if (ilce.iliskikontrolü("siparisler","kargono",dgkargolar.CurrentRow.Cells[0].Value.ToString()) != true)
+                {
+                    command.CommandText = "DELETE FROM kargo WHERE kargono=@kargono";
+                    command.Parameters.AddWithValue("@kargono", Convert.ToInt32(dgkargolar.CurrentRow.Cells[0].Value.ToString()));
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Kargo Firması Silme İşlemi Başarıyla Gerçekleştirildi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                else
+                {
+                    MessageBox.Show("Silinmek İstenen Kayıtla İlişkili Kayıtlar Bulunmaktadır.Kargo Firması Silme İşlemi İptal Edildi.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+
+            else
+            {
+                MessageBox.Show("Kargo Firması Silme İşlemi İptal Edildi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            listele();
+            temizle();
+        }
+
+
+
     }
 }
