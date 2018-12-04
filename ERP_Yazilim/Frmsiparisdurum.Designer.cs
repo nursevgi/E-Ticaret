ae4286d77a5c76bb2f180f47d1101461eee61956
nursevgi <sevgi_nur.1785@hotmail.com>
2018-12-04 09:51:53 +0300

    ERP_SiparisDurum

--- ERP_Yazilim/Frmsiparisdurum.Designer.cs
+++ ERP_Yazilim/Frmsiparisdurum.Designer.cs
@@ -28,11 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frmsiparisdurum));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btnkaydet = new System.Windows.Forms.Button();
+            this.btnyeni = new System.Windows.Forms.Button();
+            this.dgsipdur = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtsipdur = new System.Windows.Forms.TextBox();
+            this.txtsipdurid = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgsipdur)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.AliceBlue;
+            this.textBox1.Location = new System.Drawing.Point(564, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 251);
+            this.textBox1.TabIndex = 37;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // btnsil
+            // 
+            this.btnsil.BackColor = System.Drawing.Color.White;
+            this.btnsil.Image = ((System.Drawing.Image)(resources.GetObject("btnsil.Image")));
+            this.btnsil.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnsil.Location = new System.Drawing.Point(429, 103);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(79, 62);
+            this.btnsil.TabIndex = 31;
+            this.btnsil.Text = "SİL";
+            this.btnsil.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnsil.UseVisualStyleBackColor = false;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // btnkaydet
+            // 
+            this.btnkaydet.BackColor = System.Drawing.Color.White;
+            this.btnkaydet.Image = ((System.Drawing.Image)(resources.GetObject("btnkaydet.Image")));
+            this.btnkaydet.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnkaydet.Location = new System.Drawing.Point(321, 103);
+            this.btnkaydet.Name = "btnkaydet";
+            this.btnkaydet.Size = new System.Drawing.Size(79, 62);
+            this.btnkaydet.TabIndex = 29;
+            this.btnkaydet.Text = "KAYDET";
+            this.btnkaydet.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnkaydet.UseVisualStyleBackColor = false;
+            this.btnkaydet.Click += new System.EventHandler(this.btnkaydet_Click);
+            // 
+            // btnyeni
+            // 
+            this.btnyeni.BackColor = System.Drawing.Color.White;
+            this.btnyeni.Image = ((System.Drawing.Image)(resources.GetObject("btnyeni.Image")));
+            this.btnyeni.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnyeni.Location = new System.Drawing.Point(214, 103);
+            this.btnyeni.Name = "btnyeni";
+            this.btnyeni.Size = new System.Drawing.Size(79, 62);
+            this.btnyeni.TabIndex = 30;
+            this.btnyeni.Text = "YENİ";
+            this.btnyeni.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnyeni.UseVisualStyleBackColor = false;
+            this.btnyeni.Click += new System.EventHandler(this.btnyeni_Click);
+            // 
+            // dgsipdur
+            // 
+            this.dgsipdur.AllowUserToAddRows = false;
+            this.dgsipdur.AllowUserToDeleteRows = false;
+            this.dgsipdur.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgsipdur.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgsipdur.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgsipdur.Location = new System.Drawing.Point(0, 269);
+            this.dgsipdur.Name = "dgsipdur";
+            this.dgsipdur.ReadOnly = true;
+            this.dgsipdur.RowTemplate.Height = 24;
+            this.dgsipdur.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgsipdur.Size = new System.Drawing.Size(815, 510);
+            this.dgsipdur.TabIndex = 32;
+            this.dgsipdur.DoubleClick += new System.EventHandler(this.dgsipdur_DoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(248, 20);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "KAYITLI SİPARİŞ DURUMLARI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(22, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 20);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "SİPARİŞ DURUMU:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(5, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "ID:";
+            this.label1.Visible = false;
+            // 
+            // txtsipdur
+            // 
+            this.txtsipdur.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtsipdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtsipdur.Location = new System.Drawing.Point(199, 52);
+            this.txtsipdur.Name = "txtsipdur";
+            this.txtsipdur.Size = new System.Drawing.Size(323, 27);
+            this.txtsipdur.TabIndex = 28;
+            this.txtsipdur.TextChanged += new System.EventHandler(this.txtsipdur_TextChanged);
+            // 
+            // txtsipdurid
+            // 
+            this.txtsipdurid.Enabled = false;
+            this.txtsipdurid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtsipdurid.Location = new System.Drawing.Point(42, 0);
+            this.txtsipdurid.Name = "txtsipdurid";
+            this.txtsipdurid.Size = new System.Drawing.Size(34, 27);
+            this.txtsipdurid.TabIndex = 36;
+            this.txtsipdurid.TabStop = false;
+            this.txtsipdurid.Visible = false;
+            // 
+            // Frmsiparisdurum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Frmsiparisdurum";
+            this.ClientSize = new System.Drawing.Size(815, 779);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.btnkaydet);
+            this.Controls.Add(this.btnyeni);
+            this.Controls.Add(this.dgsipdur);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtsipdur);
+            this.Controls.Add(this.txtsipdurid);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(833, 826);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(833, 826);
+            this.Name = "Frmsiparisdurum";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sipariş Durumları";
+            this.Load += new System.EventHandler(this.Frmsiparisdurum_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgsipdur)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnkaydet;
+        private System.Windows.Forms.Button btnyeni;
+        private System.Windows.Forms.DataGridView dgsipdur;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtsipdur;
+        private System.Windows.Forms.TextBox txtsipdurid;
     }
 }
--- ERP_Yazilim/Frmsiparisdurum.cs
+++ ERP_Yazilim/Frmsiparisdurum.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace ERP_Yazilim
 {
@@ -15,6 +17,179 @@ namespace ERP_Yazilim
         public Frmsiparisdurum()
         {
             InitializeComponent();
+        }
+
+        SqlCommand command = new SqlCommand();
+        SqlConnection conn = new SqlConnection();
+
+        public void baglan()
+        {
+            conn.Close();
+            conn.ConnectionString = "server=.; Initial Catalog=erp;Integrated Security=SSPI";
+            command.Connection = conn;
+            command.Parameters.Clear();
+            conn.Open();
+        }
+
+        void listele()
+        {
+            baglan();
+            command.CommandText = "SELECT * FROM siparisdurum ORDER BY durum";
+            DataTable tablo = new DataTable();
+            tablo.Load(command.ExecuteReader());
+            dgsipdur.DataSource = tablo;
+            dgsipdur.Columns["sipdurno"].Visible = false;
+            dgsipdur.Columns["durum"].HeaderText = "SİPARİŞ DURUMU";
+            conn.Close();
+        }
+
+        private void Frmsiparisdurum_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        void eklesipdur()
+        {
+            baglan();
+            command.CommandText = "INSERT INTO siparisdurum (durum) VALUES (@durum)";
+            command.Parameters.AddWithValue("@durum", txtsipdur.Text);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Sipariş Durumu Başarıyla Eklendi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            conn.Close();
+
+            baglan();
+            command.CommandText = "SELECT MAX(durum) FROM siparisdurum";
+            txtsipdurid.Text = command.ExecuteScalar().ToString();
+            conn.Close();
+            listele();
+        }
+
+        void guncellesipdur()
+        {
+            baglan();
+            command.CommandText = "UPDATE siparisdurum SET durum=@durum WHERE sipdurno=@sipdurno";
+            command.Parameters.AddWithValue("@durum", txtsipdur.Text);
+            command.Parameters.AddWithValue("@sipdurno",txtsipdurid.Text);
+            command.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Sipariş Durumu Başarıyla Güncellendi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele();
+        }
+
+        void Kaydetsipdur()
+        {
+            baglan();
+            command.CommandText = "SELECT COUNT(*) FROM siparisdurum WHERE sipdurno=@sipdurno";
+            command.Parameters.AddWithValue("@sipdurno", txtsipdurid.Text);
+
+            int sayi = Convert.ToInt32(command.ExecuteScalar());
+
+            if (sayi >= 0)
+            {
+                DialogResult cevap = MessageBox.Show("Sipariş Durumu Kaydı Güncellenecektir.İşleme Devam Etmek İstiyor Musunuz?", "UYARI", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (cevap == DialogResult.Yes)
+                {
+                    guncellesipdur();
+                }
+                else
+                {
+                    MessageBox.Show("Sipariş Durumu Güncelleme İşlemi İptal Edildi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    listele();
+                }
+            }
+            else
+            {
+                eklesipdur();
+            }
+        }
+
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+            if (txtsipdur.Text != "")
+            {
+                if (FrmAnaktg.SayisalMi(txtsipdur.Text) == false)
+                {
+                    Kaydetsipdur();
+                }
+
+                else
+                {
+                    MessageBox.Show("Sipariş Durumu Sayısal Değer Olamaz!", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtsipdur.Focus();
+                    txtsipdur.SelectAll();
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Sipariş Durumu Boş Geçilemez!", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtsipdur.Focus();
+            }
+        }
+
+        private void btnyeni_Click(object sender, EventArgs e)
+        {
+            txtsipdur.Clear();
+            txtsipdurid.Clear();
+            txtsipdur.Focus();
+        }
+
+        private void txtsipdur_TextChanged(object sender, EventArgs e)
+        {
+            baglan();
+            command.CommandText = "SELECT * FROM siparisdurum WHERE durum LIKE @durum ORDER BY sipdurno";
+            command.Parameters.AddWithValue("@durum", "%" + txtsipdur.Text + "%");
+
+            DataTable tablo = new DataTable();
+            tablo.Load(command.ExecuteReader());
+            dgsipdur.DataSource = tablo;
+            conn.Close();
+        }
+
+        private void dgsipdur_DoubleClick(object sender, EventArgs e)
+        {
+            baglan();
+            command.CommandText = "SELECT * FROM siparisdurum WHERE sipdurno=@sipdurno";
+            command.Parameters.AddWithValue("@sipdurno", dgsipdur.CurrentRow.Cells[0].Value.ToString());
+
+            DataTable tablo = new DataTable();
+            tablo.Load(command.ExecuteReader());
+
+            txtsipdur.Text = tablo.Rows[0]["sipdurno"].ToString();
+            txtsipdur.Text = tablo.Rows[0]["durum"].ToString();
+            conn.Close();
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+
+            Frmilce ilce = new Frmilce();
+            baglan();
+            DialogResult cevap = MessageBox.Show("Seçilen Sipariş Durumu Kaydı Silinecektir.İşleme Devam Etmek İstiyor Musunuz?", "UYARI", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (cevap == DialogResult.Yes)
+            {
+                if (ilce.iliskikontrolü("siparisler", "sipdurno", dgsipdur.CurrentRow.Cells[0].Value.ToString()) != true)
+                {
+                    command.CommandText = "DELETE FROM siparisdurum WHERE sipdurno=@sipdurno";
+                    command.Parameters.AddWithValue("@sipdurno", Convert.ToInt32(dgsipdur.CurrentRow.Cells[0].Value.ToString()));
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Sipariş Durumu Silme İşlemi Başarıyla Gerçekleştirildi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Silinmek İstenen Kayıtla İlişkili Kayıtlar Bulunmaktadır.Sipariş Durumu Silme İşlemi İptal Edildi.İlişikili Kayıtların Bulunduğu Yer:Siparişler", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+
+            else
+            {
+                MessageBox.Show("Sipariş Durumu Silme İşlemi İptal Edildi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            listele();
         }
     }
 }
